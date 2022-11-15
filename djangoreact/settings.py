087bb2dddc830cf392a3dec24174b373d89e1ba9
pg-7v <dmkseev@gmail.com>
2022-11-15 21:07:04 +0300

    feat: added react to dj project

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party
+    'rest_framework',
+
+    # Local
+    'mainapp.apps.MainappConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'djangoreact.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']  # noqa
+        'DIRS': [BASE_DIR / 'main-ui/build',]  # noqa
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -116,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (
+    (BASE_DIR / 'main-ui/build/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
